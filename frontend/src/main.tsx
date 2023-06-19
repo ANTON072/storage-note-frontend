54d90f6ab5773344d7a9f9c7ac425f5749ebc468
Katsushi OUGI <ougi@to-r.net>
2023-06-19 16:33:50 +0900

    StrictMode戻す

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ if (import.meta.env.VITE_APP_ENV === "development") {
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
