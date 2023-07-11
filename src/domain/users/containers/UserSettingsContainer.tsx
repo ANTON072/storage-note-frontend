6c05950a4bae0ba5af68c52f2e26ae2f51492654
Katsushi OUGI <ougi@to-r.net>
2023-07-11 22:06:42 +0900

    エラー回避

--- src/domain/users/containers/UserSettingsContainer.tsx
+++ src/domain/users/containers/UserSettingsContainer.tsx
@@ -11,62 +11,63 @@ import * as yup from "yup";
 import { setError } from "@/domain/application";
 
 import { useUser } from "..";
-import { useCreateUser } from "../api/useCreateUser";
+// import { useCreateUser } from "../api/useCreateUser";
 import { UserSettingsForm } from "../components/UserSettingsForm";
 import { appUserSchema, type AppUser } from "../types";
 
 export const UserSettingsContainer = () => {
-  const { user, refetch } = useUser();
+  // const { user, refetch } = useUser();
 
-  const { onUpdateUser, isLoading } = useCreateUser();
+  // const { onUpdateUser, isLoading } = useCreateUser();
 
-  const toast = useToast();
+  // const toast = useToast();
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const defaultValues: AppUser = {
-    name: user?.name || "",
-    photoUrl: user?.photoUrl || "",
-  };
+  // const defaultValues: AppUser = {
+  //   name: user?.name || "",
+  //   photoUrl: user?.photoUrl || "",
+  // };
 
-  const form = useForm<AppUser>({
-    defaultValues,
-    resolver: yupResolver(
-      yup.object({
-        photoUrl: appUserSchema.fields.photoUrl,
-      })
-    ),
-  });
+  // const form = useForm<AppUser>({
+  //   defaultValues,
+  //   resolver: yupResolver(
+  //     yup.object({
+  //       photoUrl: appUserSchema.fields.photoUrl,
+  //     })
+  //   ),
+  // });
 
-  const onSubmit = useCallback(
-    async (values: AppUser) => {
-      try {
-        await onUpdateUser(values);
-        refetch();
-        toast({
-          title: "ユーザー情報を更新しました",
-        });
-      } catch (error) {
-        if (error instanceof AxiosError) {
-          dispatch(setError(error));
-        } else if (error instanceof FirebaseError) {
-          form.setError("photoUrl", {
-            type: "validate",
-            message: error.message,
-          });
-        } else {
-          console.error(error);
-        }
-      }
-    },
-    [dispatch, form, onUpdateUser, refetch, toast]
-  );
+  // const onSubmit = useCallback(
+  //   async (values: AppUser) => {
+  //     try {
+  //       await onUpdateUser(values);
+  //       refetch();
+  //       toast({
+  //         title: "ユーザー情報を更新しました",
+  //       });
+  //     } catch (error) {
+  //       if (error instanceof AxiosError) {
+  //         dispatch(setError(error));
+  //       } else if (error instanceof FirebaseError) {
+  //         form.setError("photoUrl", {
+  //           type: "validate",
+  //           message: error.message,
+  //         });
+  //       } else {
+  //         console.error(error);
+  //       }
+  //     }
+  //   },
+  //   [dispatch, form, onUpdateUser, refetch, toast]
+  // );
 
-  const handleSubmit = form.handleSubmit((values) => onSubmit(values));
+  // const handleSubmit = form.handleSubmit((values) => onSubmit(values));
 
   return (
-    <form onSubmit={handleSubmit}>
-      <UserSettingsForm form={form} isLoading={isLoading} />
-    </form>
+    // <form onSubmit={handleSubmit}>
+    //   <UserSettingsForm form={form} isLoading={isLoading} />
+    // </form>
+    <></>
   );
 };
