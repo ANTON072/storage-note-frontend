bd3fb9bae219c58ce0d1b804727539a111d4f160
Katsushi OUGI <ougi@to-r.net>
2023-07-12 15:13:04 +0900

    サムネイル作成フォーム

--- src/domain/application/components/FileUpload/index.tsx
+++ src/domain/application/components/FileUpload/index.tsx
@@ -1,19 +1,49 @@
+import { useEffect, useState } from "react";
+
 import { SmallAddIcon } from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/react";
+
+import { useImageEditor } from "@/domain/application";
 
 import { Image } from "./Image";
 
 export const FileUpload = () => {
+  const { ImageEditor, imageFileInputRef, croppedDataURL, isLoading } =
+    useImageEditor({
+      maxSizePx: 300 * 2,
+    });
+
+  const [tmpImage, setTmpImage] = useState("");
+
+  useEffect(() => {
+    if (croppedDataURL) {
+      setTmpImage(croppedDataURL);
+    }
+  }, [croppedDataURL]);
+
   return (
     <>
-      <Image
-        onClose={() => {
-          //
-        }}
-      />
-      <Button leftIcon={<SmallAddIcon />} width={`100%`}>
-        画像を選択
-      </Button>
+      {tmpImage ? (
+        <Image
+          w={`100%`}
+          src={tmpImage}
+          onClose={() => {
+            setTmpImage("");
+          }}
+        />
+      ) : (
+        <Button
+          leftIcon={<SmallAddIcon />}
+          width={`100%`}
+          onClick={() => {
+            imageFileInputRef.current?.click();
+          }}
+        >
+          画像を選択
+        </Button>
+      )}
+
+      <ImageEditor />
     </>
   );
 };
