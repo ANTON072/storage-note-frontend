7d256009583119b70c6cd93a4de84f86c17cc208
Katsushi OUGI <ougi@to-r.net>
2023-07-21 23:40:38 +0900

    mswの削除

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 
-// if (import.meta.env.VITE_APP_ENV === "development") {
-//   (async () => {
-//     const { worker } = await import("./mocks/browser");
-//     worker.start();
-//   })();
-// }
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
